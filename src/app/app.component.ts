f05346029ccf0c44453d2a2e3acec3979606c231
Rutuja Santikar <santikarrutuja@gmail.com>
2023-03-31 12:56:37 +0530

    property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-first-app';
 
   name='rutuja'
+
+  id='10'
+  allowNewServer=false
+ 
+  constructor(){
+  
+    setTimeout(()=>{
+       this.allowNewServer= true;
+    },2000);
+
+  }
+  ngOnInit(): void {
+    
+  }
+
 }
